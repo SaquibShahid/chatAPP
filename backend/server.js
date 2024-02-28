e096c8996df663888c5ee053f6a37616069729df
saquib <saquibshahid508@gmail.com>
2024-02-28 13:13:34 +0530

    efcesf

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-dotenv.config();
+dotenv.config({path:'../.env'});
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
